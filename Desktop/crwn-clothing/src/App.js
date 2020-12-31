1446cfb48ac3d7d0ffe99f0b6c5edb3381f43fa8
David Shin <xdavidshinx@gmail.com>
2020-12-30 16:25:45 -0800

    first commit

---
+++ Desktop/crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.js'
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
